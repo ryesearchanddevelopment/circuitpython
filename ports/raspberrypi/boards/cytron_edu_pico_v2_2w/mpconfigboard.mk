9160a494cf68db413e738e816d5d32de7b44fb53
Noqman Muzafar <noqman@cytron.io>
2025-12-23 22:31:06 +0800

    Enable sdcardio and unfreeze Adafruit_SD

--- ports/raspberrypi/boards/cytron_edu_pico_v2_2w/mpconfigboard.mk
+++ ports/raspberrypi/boards/cytron_edu_pico_v2_2w/mpconfigboard.mk
@@ -9,6 +9,8 @@ CHIP_FAMILY = rp2
 
 EXTERNAL_FLASH_DEVICES = "W25Q16JVxQ"
 
+CIRCUITPY_SDCARDIO = 1
+
 CIRCUITPY__EVE = 1
 
 CIRCUITPY_CYW43 = 1
@@ -43,7 +45,6 @@ FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_SimpleIO
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_framebuf
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_OPT4048
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_SSD1306
-FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_DisplayIO_SSD1306
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_ImageLoad
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_AHTx0
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_SD
